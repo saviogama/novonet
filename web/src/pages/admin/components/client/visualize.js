c9e7395869e8ea9584857d62db4a02b7650fa24c
J-Romulo <joseromulo.10@hotmail.com>
2020-08-11 14:44:53 -0300

    FTR11 - Tabela de pesquisa clientes/parceiros

--- web/src/pages/admin/components/client/visualize.js
+++ web/src/pages/admin/components/client/visualize.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+import {Create, Delete, Done, Close} from '@material-ui/icons'
+import {IconButton} from '@material-ui/core'
+
 import './visualize.css'
 
 export default () => {
@@ -43,92 +46,174 @@ export default () => {
         status:"Inativa",
     },
     {
-        nome:"Marcos da fé",
+        nome:"Marcos da Fé",
         codigo:"666333",
         status:"Ativo",
-    },
-    {
-        nome:"Maria do carmo",
-        codigo:"099999514",
-        status:"Inativa",
-    },
-    {
-        nome:"Marcos da fé",
-        codigo:"6663443",
-        status:"Ativo",
-    },
-    {
-        nome:"Maria do carmo da silva pereira",
-        codigo:"099999514",
-        status:"Inativa",
     }];
 
-    const [code, setCode] = useState('');
-    const [name, setName] = useState('');
     const [clients, setClients] = useState(clientes);
+    const [codeSearch, setCodeSearch] = useState('');
+    const [nameSearch, setNameSearch] = useState('');
 
-    function handleCode(e){
-        setCode(e);
+    const [edit, setEdit] = useState(false);
+    const [indexTableEdit, setIndexTableEdit] = useState('');
+    const [nameEditable, setNameEditable] = useState('');
 
-        if(name === '' && e === ''){
+    function openEditTablesIndex(index){
+        setEdit(true);
+        setIndexTableEdit(index);
+        setNameEditable(clients[index].nome);
+    }
+
+    function cancelEdit(){
+        setEdit(false);
+    }
+
+    function deleteTablesIndex(){
+        console.log('Delete');
+    }
+
+    function searchByCode(codePassedForSearch){
+        setCodeSearch(codePassedForSearch);
+
+        if(nameSearch === '' && codePassedForSearch === ''){
             setClients(clientes);
-        }else if(e !== '' && name !== ''){
-            setClients(clientes.filter((client, index, array) => client.codigo === e && client.nome === name));
-        }else if(e === '' && name !== ''){
-            console.log(name);
-            setClients(clientes.filter((client, index, array) => client.nome === name));
+        }else if(codePassedForSearch !== '' && nameSearch !== ''){
+            setClients(clientes.filter((client, index, array) => client.codigo === codePassedForSearch && client.nome === nameSearch));
+        }else if(codePassedForSearch === '' && nameSearch !== ''){
+            console.log(nameSearch);
+            setClients(clientes.filter((client, index, array) => client.nome === nameSearch));
         }else{
-            setClients(clientes.filter((client, index, array) => client.codigo === e));
+            setClients(clientes.filter((client, index, array) => client.codigo === codePassedForSearch));
         }
 
     }
 
-    function handleNameSearch(e){
-        setName(e);
+    function searchByName(namePassedForSearch){
+        setNameSearch(namePassedForSearch);
 
-        if(e === '' && code === ''){
+        if(namePassedForSearch === '' && codeSearch === ''){
             setClients(clientes);
-        }else if(e !== '' && code !== ''){
-            setClients(clientes.filter((client, index, array) => client.nome === e && client.codigo === code));
-        }else if(e === '' && code !== ''){
-            setClients(clientes.filter((client, index, array) => client.codigo === code));
+        }else if(namePassedForSearch !== '' && codeSearch !== ''){
+            setClients(clientes.filter((client, index, array) => client.nome === namePassedForSearch && client.codigo === codeSearch));
+        }else if(namePassedForSearch === '' && codeSearch !== ''){
+            setClients(clientes.filter((client, index, array) => client.codigo === codeSearch));
         }else{
-            setClients(clientes.filter((client, index, array) => client.nome === e));
+            setClients(clientes.filter((client, index, array) => client.nome === namePassedForSearch));
         }
     }
 
-
-
-    return(
-        <div className="container-visualize_cliente">
-            <div className="search-container">
-                <input className="input-code" type="text" placeholder="Código do cliente" value={code} onChange={e => handleCode(e.target.value)}/>
-                <input className="input-name" type="text" placeholder="Nome do cliente" value={name} onChange={e => handleNameSearch(e.target.value)}/>
+    if(!edit){
+        return(
+            <div className="container-visualize">
+    
+                <div className="search-container">
+                    <input className="input-name" type="text" placeholder="Nome do cliente" value={nameSearch} onChange={e => searchByName(e.target.value)}/>
+                    <input className="input-code" type="text" placeholder="Código do cliente" value={codeSearch} onChange={e => searchByCode(e.target.value)}/>
+                </div>
+    
+                <div className="table-container">
+                    <table className="table-clients">
+                        <thead>
+                            <tr className="header-table-client">
+                                <th>Nome</th>
+                                <th>Código</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+    
+                        <tbody>
+                            {
+                                clients.map((client, index) => (
+                                    <tr key={index} className="data-row-client">
+                                        <td>{client.nome}</td>
+                                        <td>{client.codigo}</td>
+                                        <td>{client.status}</td>
+                                        <td className="icon">
+                                            <IconButton onClick={() => openEditTablesIndex(index)}>
+                                                <Create/>
+                                            </IconButton>
+                                        </td>
+                                        <td className="icon">
+                                            <IconButton onClick={() => deleteTablesIndex()}>
+                                                <Delete/>
+                                            </IconButton>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-            <div className="table-container">
-                <table className="table-clients">
-                    <thead>
-                        <tr className="header-table">
-                            <th>Nome</th>
-                            <th>Código</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {
-                            clients.map(client => (
-                                <tr className="data-row">
-                                    <td>{client.nome}</td>
-                                    <td>{client.codigo}</td>
-                                    <td>{client.status}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+        );
+    }else{
+        return(
+            <div className="container-visualize">
+    
+                <div className="search-container">
+                    <input className="input-name" type="text" placeholder="Nome do cliente" value={nameSearch} onChange={e => searchByName(e.target.value)}/>
+                    <input className="input-code" type="text" placeholder="Código do cliente" value={codeSearch} onChange={e => searchByCode(e.target.value)}/>
+                </div>
+    
+                <div className="table-container">
+                    <table className="table-clients">
+                        <thead>
+                            <tr className="header-table-client">
+                                <th>Nome</th>
+                                <th>Código</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+    
+                        <tbody>
+                            {
+                                clients.map((client, index) => {
+                                    if(index === indexTableEdit){
+                                        return(
+                                            <tr key={index} className="data-row-client">
+                                                <td><input type="text" value={nameEditable} onChange={e => setNameEditable(e.target.value)}></input></td>
+                                                <td>{client.codigo}</td>
+                                                <td>{client.status}</td>
+                                                <td className="icon">
+                                                    <IconButton>
+                                                        <Done/>
+                                                    </IconButton>
+                                                </td>
+                                                <td className="icon">
+                                                    <IconButton onClick={() => cancelEdit()}>
+                                                        <Close/>
+                                                    </IconButton>
+                                                </td>
+                                            </tr>
+                                        );
+                                    }else{
+                                        return(
+                                            <tr key={index} className="data-row-client">
+                                                <td>{client.nome}</td>
+                                                <td>{client.codigo}</td>
+                                                <td>{client.status}</td>
+                                                <td className="icon">
+                                                    <IconButton onClick={() => openEditTablesIndex(index)}>
+                                                        <Create/>
+                                                    </IconButton>
+                                                </td>
+                                                <td className="icon">
+                                                    <IconButton>
+                                                        <Delete/>
+                                                    </IconButton>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
+    
 }
--- web/src/pages/admin/components/partner/visualize.js
+++ web/src/pages/admin/components/partner/visualize.js
@@ -1,9 +1,214 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Create, Delete, Done, Close} from '@material-ui/icons'
+import {IconButton} from '@material-ui/core'
+
+import './visualize.css'
 
 export default () => {
-    return(
-        <div>
-            <h1>Visualizar parceiros</h1>
-        </div>
-    );
+    const parceiros = [{
+        nome:"Academia X",
+        email:"X@hot.com",
+        CNPJ:"00012145235",
+        endereco:"São Paulo - SP",
+        telefone:"99778899"
+    },
+    {
+        nome:"Maria",
+        email:"maria13@hot.com",
+        CNPJ:"0001123545",
+        endereco:"São Paulo - SP",
+        telefone:"99778899"
+    },
+    {
+        nome:"Marina do Carmo",
+        email:"Mari@gmail.com",
+        CNPJ:"0001216879",
+        endereco:"São Paulo - SP",
+        telefone:"99778899"
+    },
+    {
+        nome:"Robson almeida",
+        email:"robsonalmeida@bol.com",
+        CNPJ:"552416213",
+        endereco:"Recife - PE",
+        telefone:"81 99665588"
+    },
+    ];
+
+    const [cnpjSearch, setCNPJSearch] = useState('');
+    const [nameSearch, setNameSearch] = useState('');
+    const [partners, setPartners] = useState(parceiros);
+
+    const [edit, setEdit] = useState(false);
+    const [indexTableEdit, setIndexTableEdit] = useState('');
+    const [nameEditable, setNameEditable] = useState('');
+
+    function openEditTablesIndex(index){
+        setEdit(true);
+        setIndexTableEdit(index);
+        setNameEditable(partners[index].nome);
+    }
+
+    function cancelEdit(){
+        setEdit(false);
+    }
+
+    function deleteTablesIndex(){
+        console.log('Delete');
+    }
+
+    function searchByCnpj(passedCnpjForSearch){
+        setCNPJSearch(passedCnpjForSearch);
+
+        if(nameSearch === '' && passedCnpjForSearch === ''){
+            setPartners(parceiros);
+        }else if(passedCnpjForSearch !== '' && nameSearch !== ''){
+            setPartners(parceiros.filter((partners) => partners.CNPJ === passedCnpjForSearch && partners.nome === nameSearch));
+        }else if(passedCnpjForSearch === '' && nameSearch !== ''){
+            console.log(nameSearch);
+            setPartners(parceiros.filter((partners) => partners.nome === nameSearch));
+        }else{
+            setPartners(parceiros.filter((partners) => partners.CNPJ === passedCnpjForSearch));
+        }
+
+    }
+
+    function searchByName(passedNameForSearch){
+        setNameSearch(passedNameForSearch);
+
+        if(passedNameForSearch === '' && cnpjSearch === ''){
+            setPartners(parceiros);
+        }else if(passedNameForSearch !== '' && cnpjSearch !== ''){
+            setPartners(parceiros.filter((partners) => partners.nome === passedNameForSearch && partners.CNPJ === cnpjSearch));
+        }else if(passedNameForSearch === '' && cnpjSearch !== ''){
+            setPartners(parceiros.filter((partners) => partners.CNPJ === cnpjSearch));
+        }else{
+            setPartners(parceiros.filter((partners) => partners.nome === passedNameForSearch));
+        }
+    }
+
+    if(!edit){
+        return(
+            <div className="container-visualize">
+    
+                <div className="search-container">
+                    <input className="input-name" type="text" placeholder="Nome do parceiro" value={nameSearch} onChange={e => searchByName(e.target.value)}/>
+                    <input className="input-code" type="text" placeholder="CNPJ" value={cnpjSearch} onChange={e => searchByCnpj(e.target.value)}/>
+                </div>
+    
+                <div className="table-container">
+                    <table className="table-clients">
+                        <thead>
+                            <tr className="header-table-partner">
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>CNPJ</th>
+                                <th>Endereço</th>
+                                <th>Telefone</th>
+                            </tr>
+                        </thead>
+    
+                        <tbody>
+                            {
+                                partners.map((partner, index) => (
+                                    <tr key={index} className="data-row-partner">
+                                        <td>{partner.nome}</td>
+                                        <td>{partner.email}</td>
+                                        <td>{partner.CNPJ}</td>
+                                        <td>{partner.endereco}</td>
+                                        <td>{partner.telefone}</td>
+                                        <td className="icon">
+                                            <IconButton onClick={() => openEditTablesIndex(index)}>
+                                                <Create/>
+                                            </IconButton>
+                                        </td>
+                                        <td className="icon">
+                                            <IconButton onClick={() => deleteTablesIndex()}>
+                                                <Delete/>
+                                            </IconButton>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }else{
+        return(
+            <div className="container-visualize">
+    
+                <div className="search-container">
+                    <input className="input-name" type="text" placeholder="Nome do parceiro" value={nameSearch} onChange={e => searchByName(e.target.value)}/>
+                    <input className="input-code" type="text" placeholder="Código do parceiro" value={cnpjSearch} onChange={e => searchByCnpj(e.target.value)}/>
+                </div>
+    
+                <div className="table-container">
+                    <table className="table-clients">
+                        <thead>
+                            <tr className="header-table-partner">
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>CNPJ</th>
+                                <th>Endereço</th>
+                                <th>Telefone</th>
+                            </tr>
+                        </thead>
+    
+                        <tbody>
+                            {
+                                partners.map((partner, index) => {
+                                    if(index === indexTableEdit){
+                                        return(
+                                            <tr key={index} className="data-row-partner">
+                                                <td><input type="text"  value={nameEditable} onChange={e => setNameEditable(e.target.value)}></input></td>
+                                                <td>{partner.email}</td>
+                                                <td>{partner.CNPJ}</td>
+                                                <td>{partner.endereco}</td>
+                                                <td>{partner.telefone}</td>
+
+                                                <td className="icon">
+                                                    <IconButton>
+                                                        <Done/>
+                                                    </IconButton>
+                                                </td>
+                                                <td className="icon">
+                                                    <IconButton onClick={() => cancelEdit()}>
+                                                        <Close/>
+                                                    </IconButton>
+                                                </td>
+                                            </tr>
+                                        );
+                                    }else{
+                                        return(
+                                            <tr key={index} className="data-row-partner">
+                                                <td>{partner.nome}</td>
+                                                <td>{partner.email}</td>
+                                                <td>{partner.CNPJ}</td>
+                                                <td>{partner.endereco}</td>
+                                                <td>{partner.telefone}</td>
+                                                <td className="icon">
+                                                    <IconButton onClick={() => openEditTablesIndex(index)}>
+                                                        <Create/>
+                                                    </IconButton>
+                                                </td>
+                                                <td className="icon">
+                                                    <IconButton>
+                                                        <Delete/>
+                                                    </IconButton>
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+    
+    
 }
