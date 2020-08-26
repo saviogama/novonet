88832a6d750cc69e0f45089d5a5c7cee131e4c49
WevertonCintra <wevertoncintra@outlook.com>
2020-08-26 18:52:36 -0300

    FTR00 - corretion

--- server/src/app/controllers/Admin/ImportFileController.js
+++ server/src/app/controllers/Admin/ImportFileController.js
@@ -1,26 +1,26 @@
-// import Admin from '../../models/Admin';
+import Admin from '../../models/Admin';
 
-// import ImportFileService from '../../services/ImportFileService';
+import ImportFileService from '../../services/ImportFileService';
 
-// class ImportFileController {
-//   async store(request, response) {
-//     const admin = await Admin.findOne({
-//       where: {
-//         id: request.userId,
-//         admin_type: true,
-//       },
-//     });
+class ImportFileController {
+  async store(request, response) {
+    const admin = await Admin.findOne({
+      where: {
+        id: request.userId,
+        admin_type: true,
+      },
+    });
 
-//     if (!admin) {
-//       return response
-//         .status(400)
-//         .json({ error: 'You do not have access to this functionality!' });
-//     }
+    if (!admin) {
+      return response
+        .status(400)
+        .json({ error: 'You do not have access to this functionality!' });
+    }
 
-//     const uploadData = await ImportFileService.run(request.file.path);
+    const uploadData = await ImportFileService.run(request.file.path);
 
-//     return response.json(uploadData);
-//   }
-// }
+    return response.json(uploadData);
+  }
+}
 
-// export default new ImportFileController();
+export default new ImportFileController();
