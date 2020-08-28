f188c55c58954a434bc8ce66bccdf67671531e6e
J-Romulo <joseromulo.10@hotmail.com>
2020-08-28 11:43:19 -0300

    HTF00 - Registro de cliente/admin

--- web/src/components/pages/admin/components/client/register.js
+++ web/src/components/pages/admin/components/client/register.js
@@ -10,6 +10,7 @@ export default () => {
     const [email, setEmail] = useState('');
     const [rg, setRG] = useState('');
     const [cpf, setCPF] = useState('');
+    const [code, setCode] = useState('');
 
     const token = JSON.parse(tokenAdmin());
 
@@ -17,7 +18,7 @@ export default () => {
         e.preventDefault();
         try{
             api.defaults.headers.Authorization = `Bearer ${token}`;
-            await api.post('/clients', {"email": email, "firstname": name, "lastname": lastName, "rg": rg, "cpf":cpf})
+            await api.post('/clients', {"email": email, "firstname": name, "lastname": lastName, "rg": rg, "cpf":cpf, "code": code})
 
             resetFields();
             alert('Cadastro de cliente concluido!');
@@ -32,6 +33,7 @@ export default () => {
         setEmail('');
         setRG('');
         setCPF('');
+        setCode('');
     }
     return(
         <div className="container">
@@ -39,9 +41,9 @@ export default () => {
             <form className="form-container" onSubmit={handleSubmit}>
                 <div className="inputs-container">
                     <div className="inputs-coluna">
-                        <input className="input-nome" type="text" placeholder="Nome (Min. 5)" value={name} onChange={e => setName(e.target.value)}/>
+                        <input className="input-nome" type="text" placeholder="Nome" value={name} onChange={e => setName(e.target.value)}/>
 
-                        <input className="input-lastName" type="text" placeholder="Sobrenome (Min. 6)" value={lastName} onChange={e => setLastName(e.target.value)}/>
+                        <input className="input-lastName" type="text" placeholder="Sobrenome" value={lastName} onChange={e => setLastName(e.target.value)}/>
 
                         <input className="input-email" type="email" placeholder="Email (Min. 10)" value={email} onChange={e => setEmail(e.target.value)}/>
                     </div>
@@ -50,6 +52,8 @@ export default () => {
                         <input className="input-rg" type="text" placeholder="RG" value={rg} onChange={e => setRG(e.target.value)}/>
 
                         <input className="input-cpf" type="text" placeholder="CPF" value={cpf} onChange={e => setCPF(e.target.value)}/>
+
+                        <input className="input-code" type="text" placeholder="Código de cliente" value={code} onChange={e => setCode(e.target.value)}/>
                     </div>
                 </div>
 
